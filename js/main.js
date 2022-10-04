9a87db0842ef8007b54e7bed11fb22f9a3575155
Avim4x <104305037+Avim4x@users.noreply.github.com>
2022-10-04 17:04:59 +0500

    Добавил функции в проект

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 function getRandomNumber (min, max){
-    return Math.floor(Math.random() * (max - min)) + min;
-    // Взято с сайта (https://myrusakov.ru/js-random-numbers.html)
+  return Math.floor(Math.random() * (max - min)) + min;
+  // Взято с сайта (https://myrusakov.ru/js-random-numbers.html)
 }
 function lenghtOfComment (string = '', maxLenght = 140) {
-    if (string.length <= maxLenght){
-        return true;
-    }
-    return false;
+  if (string.length <= maxLenght){
+    return true;
+  }
+  return false;
 }
 getRandomNumber();
 lenghtOfComment();
