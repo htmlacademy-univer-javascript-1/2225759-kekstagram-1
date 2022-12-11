a687293d321dafaf2ed76e5e86e5b1b81c10ca09
Avim4x <104305037+Avim4x@users.noreply.github.com>
2022-12-11 20:14:42 +0500

    module7-task1

--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+//Генерация числа из массива
+
 function getRandomPositiveInteger (a, b) {
     const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
     const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -5,9 +7,22 @@ function getRandomPositiveInteger (a, b) {
     return Math.floor(result);
   }
   
-  const getRandomArrayElement = (elements) => {
-    return elements[getRandomPositiveInteger(0, elements.length - 1)];
+  // Функция для проверки максимальной длины строки.
+  
+  function checkStringLength (string, length) {
+    return string.length <= length;
+  }
+  
+  checkStringLength('строка', 5);
+  
+  // Функция закрытия окна по Escape
+  
+  const getEscapeEvent = (evt, action) => {
+    if (evt.key === 'Escape') {
+      action();
+    }
   };
   
-  export {getRandomArrayElement};
   export {getRandomPositiveInteger};
+  export {checkStringLength};
+  export {getEscapeEvent};
