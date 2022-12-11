59ab15189c8a79afb6eac6822ee3e0f6c214c929
Avim4x <104305037+Avim4x@users.noreply.github.com>
2022-12-11 20:16:21 +0500

    module7-task2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
-function getRandomNumber (min, max){
-  return Math.floor(Math.random() * (max - min)) + min;
-  // Взято с сайта (https://myrusakov.ru/js-random-numbers.html)
-}
-function lenghtOfComment (string = '', maxLenght = 140) {
-  if (string.length <= maxLenght){
-    return true;
-  }
-  return false;
-}
-getRandomNumber();
-lenghtOfComment();
+import {getObjectOutput} from './data.js';
+import {createPhotosFragment} from './picture.js';
 
+const photosData = getObjectOutput();
+createPhotosFragment(photosData);
